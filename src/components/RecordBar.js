9e08d602691e4025f350c856d1d7b8dadd3ca351
prethrolic <re.prestige@windowslive.com>
2020-12-01 04:47:34 +0700

    feat: add modal layout

--- src/components/RecordBar.js
+++ src/components/RecordBar.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import axios from 'axios'
+import Scoreboard from './Scoreboard'
 import WordSelector from './WordSelector'
 import { Row, Col, Button } from 'react-bootstrap'
 import { ReactMediaRecorder } from "react-media-recorder"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faMicrophone, faCircleNotch} from '@fortawesome/free-solid-svg-icons'
+import { faMicrophone, faCircleNotch, faSpellCheck } from '@fortawesome/free-solid-svg-icons'
 import '../scss/recordbar.scss'
-import axios from 'axios'
 
 
-library.add(faMicrophone, faCircleNotch);
+library.add(faMicrophone, faCircleNotch, faSpellCheck);
 
 
 class RecordBar extends React.Component {
@@ -18,11 +19,13 @@ class RecordBar extends React.Component {
     this.state = {
       quote: this.props.quote,
       selectedIndices: this.props.quote.script.map((_, idx) => idx),
-      score: "Score",
+      score: null,
+      evaluationEnabled: false,
     }
 
     this.onSelectedWordChanged = this.onSelectedWordChanged.bind(this)
     this.getEvaluation = this.getEvaluation.bind(this)
+    this.enableEvaluation = this.enableEvaluation.bind(this)
   }
 
   componentDidUpdate = (prevProps) => {
@@ -30,6 +33,8 @@ class RecordBar extends React.Component {
       this.setState({
         quote: this.props.quote,
         selectedIndices: this.props.quote.script.map((_, idx) => idx),
+        score: null,
+        evaluationEnabled: false,
       })
     }
   }
@@ -51,50 +56,58 @@ class RecordBar extends React.Component {
     axios.post('https://143.248.150.127:8080/score', {user}).then(
       res => this.setState({score: res.data.username})
       );
+  }
 
+  enableEvaluation = () => {
+    this.setState({ evaluationEnabled: true })
   }
 
   render() {
     return(
-      <div className="record-bar">
-        <WordSelector quote={this.state.quote.script} selectedIndices={this.state.selectedIndices} onSelectedWordChanged={this.onSelectedWordChanged} />
-        <div className="justify-content-md-center">
-          <ReactMediaRecorder
-            audio
-            render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
-              <div className="record-bar--wrap">
+      <Row className="record-bar no-gutters">
+        <Col>
+          <WordSelector quote={this.state.quote.script} selectedIndices={this.state.selectedIndices} onSelectedWordChanged={this.onSelectedWordChanged} />
+          <div className="justify-content-md-center">
+            <ReactMediaRecorder
+              audio
+              render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
+                <div className="record-bar--wrap">
                   {
-                    status === "recording" ? 
-                      <Button
-                        bsPrefix="record-bar--round-button active"
-                        onClick={() => {stopRecording()}}
-                      >
-                          <FontAwesomeIcon icon="microphone" size="lg" />
-                      </Button>
-                      :
-                      status === "stopping" ?
-                      <Button
-                        bsPrefix="record-bar--round-button loading"
-                      >
-                          <FontAwesomeIcon icon="circle-notch" size="lg" spin />
-                      </Button>
-                      :
-                      <Button
-                        bsPrefix="record-bar--round-button inactive"
-                        onClick={startRecording}
-                      >
-                          <FontAwesomeIcon icon="microphone" size="lg" />
-                      </Button>
+                  status === "recording" ? 
+                    <Button
+                      bsPrefix="record-bar--round-button active"
+                      onClick={() => {stopRecording(); this.enableEvaluation(); }}
+                    >
+                        <FontAwesomeIcon icon="microphone" size="lg" />
+                    </Button>
+                    :
+                    status === "stopping" ?
+                    <Button bsPrefix="record-bar--round-button loading">
+                        <FontAwesomeIcon icon="circle-notch" size="lg" spin />
+                    </Button>
+                    :
+                    <Button
+                      bsPrefix="record-bar--round-button inactive"
+                      onClick={startRecording}
+                    >
+                        <FontAwesomeIcon icon="microphone" size="lg" />
+                    </Button>
                   }
-                  <audio className="record-bar--player" src={mediaBlobUrl} controls />
-                  <Button onClick={() => {this.getEvaluation("jyp")}}>
-                    {this.state.score}
+                  <Button
+                    bsPrefix={"record-bar--round-button long " +  (this.state.evaluationEnabled ? "inactive" : "disabled")}
+                    onClick={() => {this.getEvaluation("jyp")}}
+                  >
+                      <FontAwesomeIcon icon="spell-check" size="lg" />
+                      <div className="record-bar--round-button--label">evaluate</div>
                   </Button>
-              </div>
-            )}
-          />
-        </div>
-      </div>
+                  <audio className="record-bar--player" src={mediaBlobUrl} controls />
+                </div>
+              )}
+            />
+          </div>
+        </Col>
+        <Scoreboard />
+      </Row>
     );
   }
 }
